bc4b5e4b758e9b9516ed2436c162ae7fb9620617
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-16 09:19:42 +0100

    Adds styled-components for Code styling

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -4,10 +4,11 @@ import Highlight, {
   Language,
   PrismTheme,
 } from 'prism-react-renderer'
+import styled from 'styled-components'
 import DefaultPrismTheme from 'prism-react-renderer/themes/nightOwl'
 
 export interface CodeProps {
-  children: (api: {
+  children?: (api: {
     Preview: React.FC
     focusLines: (index: number[]) => void
     copyToClipboard: () => Promise<void>
@@ -21,6 +22,24 @@ export interface CodeProps {
   focusedLines?: number[]
 }
 
+const Pre = styled.pre<{ showLineNumbers?: boolean }>`
+  border-radius: 3px;
+  font-size: 1rem;
+  line-height: 1.4;
+  text-align: start;
+  padding: ${({ showLineNumbers }) =>
+    showLineNumbers ? '1rem 1rem 1rem 0' : '1rem'};
+`
+
+const LineNumber = styled.span`
+  display: inline-flex;
+  justify-content: flex-end;
+  padding-right: 0.5rem;
+  width: 2em;
+  user-select: none;
+  opacity: 0.5;
+`
+
 export const Code: React.FC<CodeProps> = ({
   children = ({ Preview }) => <Preview />,
   code,
@@ -30,8 +49,19 @@ export const Code: React.FC<CodeProps> = ({
   lineNumberStart = 1,
   focusedLines = [],
 }) => {
-  const [activeFocusedLines, focusLines] = React.useState<number[]>(focusedLines)
+  const [activeFocusedLines, focusLines] = React.useState<number[]>(
+    focusedLines
+  )
   const normalizedCode = React.useMemo(() => code.trim(), [code])
+
+  const getLineNumberClass = (lineIndex: number) => {
+    return [
+      'line-number',
+      activeFocusedLines.includes(lineIndex) && 'line-number-focused',
+    ]
+      .filter(Boolean)
+      .join(' ')
+  }
 
   const Preview = () => (
     <Highlight
@@ -41,36 +71,36 @@ export const Code: React.FC<CodeProps> = ({
       theme={theme}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style}>
-          {showLineNumbers && (
+        <Pre
+          className={className}
+          style={style}
+          showLineNumbers={showLineNumbers}
+        >
+          {/* {showLineNumbers && (
             <div>
               {tokens.map((_line, lineIndex) => (
-                <div
-                  className="line-number"
-                  data-line-focused={
-                    activeFocusedLines.includes(lineIndex) ? 'true' : 'false'
-                  }
-                >
+                <div key={lineIndex} className={getLineNumberClass(lineIndex)}>
                   {lineIndex + lineNumberStart}
                 </div>
               ))}
             </div>
-          )}
+          )} */}
           <div>
             {tokens.map((line, lineIndex) => (
               <div
                 {...getLineProps({ line, key: lineIndex })}
-                data-line-focused={
-                  activeFocusedLines.includes(lineIndex) ? 'true' : 'false'
-                }
+                className={getLineNumberClass(lineIndex)}
               >
+                {showLineNumbers && (
+                  <LineNumber>{lineIndex + lineNumberStart}</LineNumber>
+                )}
                 {line.map((token, key) => (
                   <span {...getTokenProps({ token, key })} />
                 ))}
               </div>
             ))}
           </div>
-        </pre>
+        </Pre>
       )}
     </Highlight>
   )
